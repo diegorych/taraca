e5ff724aa9b6426dbcab47db0ff712fea5a63994
diegorych <39017382+diegorych@users.noreply.github.com>
2026-04-13 19:46:59 +0200

    m

--- .next/dev/server/app/page.js
+++ .next/dev/server/app/page.js
@@ -1,7 +1,7 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/app/page.js")
 R.c("server/chunks/ssr/node_modules_next_dist_2125e029._.js")
 R.c("server/chunks/ssr/[root-of-the-server]__b17515da._.js")
-R.c("server/chunks/ssr/node_modules_e2d1c5df._.js")
+R.c("server/chunks/ssr/node_modules_next_dist_eac56746._.js")
 R.c("server/chunks/ssr/[externals]__e8a2741f._.js")
 R.c("server/chunks/ssr/app_b9b1292a._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_client_components_builtin_global-error_ece394eb.js")
